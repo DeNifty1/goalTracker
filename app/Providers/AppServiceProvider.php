0a49c11d63cbe50dad3ec07eb4123f69864abe2d
DeNifty1 <chris@bugert.com>
2018-01-01 11:29:05 -0500

    Code that keeps Migration tool from throwing errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //Added because of error
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //Added because of error
     }
 
     /**
